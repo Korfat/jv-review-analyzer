6db9d742131528372f3666592944f571f191af7a
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-11-02 15:48:03 +0200

    init commit

---
+++ src/main/java/mate/academy/jvreviewanalyzer/JvReviewAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package mate.academy.jvreviewanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvReviewAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvReviewAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mate/academy/jvreviewanalyzer/JvReviewAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy.jvreviewanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvReviewAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
